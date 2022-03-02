1c56b1dbe9b2dfa1e5057531f79baa782b9b9f23
Vicente <vicente.ict3@gmail.com>
2022-03-02 18:57:18 +0100

    Enrutamiento añadido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren:() => import(`./modules/auth/auth.module`).then(m => m.AuthModule)
+  },
+  {
+    path: '',
+    loadChildren:() => import(`./modules/home/home.module`).then(m => m.HomeModule)
+  },
+  {
+    path: 'game',
+    loadChildren:() => import(`./modules/game/game.module`).then(m => m.GameModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
